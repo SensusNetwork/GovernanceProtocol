efb2a6515ffbb7c5ea4542d3320ae9c290274e0b
SensusNetwork <39761019+SensusNetwork@users.noreply.github.com>
2018-05-31 16:39:08 +0100

    Create ProxyStorageMock.sol

---
+++ ProxyStorageMock.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.18;
+import '../../contracts/ProxyStorage.sol';
+
+contract ProxyStorageMock is ProxyStorage {
+    function ProxyStorageMock(address _SensusConsensus)
+    ProxyStorage(_sensusConsensus)
+  {
+  }
+    uint256 public time;
+    function setTime(uint256 _time) public {
+        time = _time;
+    }
+
+    function getTime() public view returns(uint256) {
+      if(time == 0) {
+        return now;
+      } else {
+        return time;
+      }
+    }
+
+
+    function setVotingContractMock(address _newAddress) public {
+        votingToChangeKeys = _newAddress;
+    }
+
+    function setVotingToChangeMinThresholdMock(address _newAddress) public {
+        votingToChangeMinThreshold = _newAddress;
+   
